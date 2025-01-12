27f7a328c276832f1d70e9949eb97b05e92de097
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-01-12 17:02:58 -0500

    - Completed out-lab assignment 1.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # NE591
-Repository for in-Lab and out-Lab assignments.
+Repository for in-Lab and out-Lab assignments of NE:591 - Mathematical and Computational methods in NE.
+
+#### a) To compile the code:
+        1. Go inside the decompressed outlab1 folder and open the terminal in that directory.
+        2. Type in the following commands in the terminal:
+           ```mkdir build```
+           ```cd build```
+           ```cmake -S .. -B .```
+           ```make```
+
+#### b) To run the code:
+        1. Run the following command in terminal:
+           ```./outlab1```
+        2. Then enter the values as you wish!
+
+#### c) The code is `Operational`.
+
+#### d) The code can be explained in the following steps:
+        Step 1: The code asks for the constant value k. Matrix A is multiplied by k later on, and the resultant matrix
+                is stored in matrix D. Then the code asks for 3 values: M, N and J. The dimension of matrices A and B
+                are (M x N), meaning there are M number of rows and N number of columns. The dimension of the F matrix
+                is (N x J).
+        Step 2: If the user provided dimensions of the matrices are non-positive, then the code throws an error saying
+                that the dimensions have to be bigger than 0.
+        Step 3: According to the user provided dimensions, the code then resizes the already variables (A, B, F) that
+                are used for storing the original (A, B, F) and resultant (C, D, E) matrices.
+        Step 4: The original matrices (A, B, F) are then populated using a nested loop that has 2 loops in it. The outer
+                loop increments the value `m/n` until the maximum row number is reached and the inner loop increments the
+                value `n/j` until the maximum column number is reached.
+                Matrix A -> The diagonal values have been assigned to 1.0 and the rest of the values are equal to 0.5
+                Matrix B -> If the row number is bigger than the column number then the value is 0.75, otherwise 0.25
+                Matrix F -> All the values are equal to { 1 / (row no. + column no.) } except the first one, since the
+                first values is indexed at (0, 0), which will result in { 1 / (0 + 0) } or, 1 / 0 = infinity. So instead
+                1 is added to the summation of n + j.
+        Step 5: Matrix addition is performed simply by looping through the rows(m) of matrices A and B in the outer loop
+                and through the columns (n) in the inner loop. The operation follows this simple formula:
+                C(m, n) = A(m, n) + B(m, n)
+        Step 6: Matrix A is multiplied by the constant value k using the formula:
+                D(m, n) = A(m, n) * k;
+        Step 7: Suppose the dimension of matrix A is (2 x 3) and the dimension of F is (3 x 4)
+                In this case, the dimension of the resultant matrix, E = A x F, will be (2 x 4)
+                and, the total number of multiplication operations will be 2 x 3 x 4 = 24
+                So, we have a nested loop with 3 loops in it. The outer loop changes according to the row number of A.
+                The second loop changes according to the column number of A or row number of F. Since, according to our
+                example, the resultant matrix will have 8 elements in it due to its 2 x 4 dimension. So that means, if
+                we loop through these first 2 loops, we will make 2 x 4 = 8 operations. So whatever summation we get in
+                the innermost loop, we assign that value to the E matrix, exactly 8 times.
+                The innermost loop performs the actual matrix multiplication using the following formula:
+                E(m, j) = sum of all A(m, n) x F(n, j) where m, n, j = 1 to M, N, J respectively.
+                According to our example, the outer loop runs 2 times, the second loop runs 3 times, and the inner loop
+                runs 4 times, doing exactly (2 x 3 x 4) = 24 multiplication operations.
+        Step 8: We loop through the rows and columns of each resultant matrices (C, D and E) and print the results in a
+                presentable way.
--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
  * Created by Hasibul Hossain Rasheeq on Sunday, 1/12/25.
 */
 
+/*----------------------------VARIABLE DECLARATION----------------------------*/
+/*
+ * k - Stores the constant that will be multiplied with Matrix A.
+ * M, N - Stores the number of rows and columns respectively, of matrix A and B.
+ * N, J - Stores the number of rows and columns of matrix F.
+ * A, B, F - Stores the generated matrices A, B, and F.
+ * C - Stores the resultant matrix of operation A + B
+ * D - Stores the resultant matrix of operation k.A
+ * E - Stores the resultant matrix of operation A x F
+ */
+/*-------------------------VARIABLE DECLARATION ENDS--------------------------*/
+
+#include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
 int main() {
@@ -18,17 +32,28 @@ int main() {
     std::vector<std::vector<double>> D;
     std::vector<std::vector<double>> E;
 
+    // Step 1: Prompting the user for providing the values of k, M, N and J
     std::cout << "Please enter a real value (k): " << std::endl;
     std::cin >> k;
     std::cout << "Please enter the dimensions (M, N, J) for the matrices: " << std::endl;
     std::cin >> M >> N >> J;
 
+    // Step 2: Throwing an error in case one of the user provided dimensions is non-positive
     if (M <= 0 || N <= 0 || J <= 0) {
         throw std::invalid_argument("Invalid dimensions! Dimensions have to be bigger than 0");
     } else {
         std:: cout << "There are no errors in the input! Give me a few moments to perform the Matrix operations!" << std::endl;
     }
 
+    // Step 3: Resize matrices according to the user provided dimensions
+    A.resize(M, std::vector<double>(N));
+    B.resize(M, std::vector<double>(N));
+    C.resize(M, std::vector<double>(N));
+    D.resize(M, std::vector<double>(N));
+    E.resize(M, std::vector<double>(J));
+    F.resize(N, std::vector<double>(J));
+
+    // Step 4: Populate the matrices
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
             if (m == n) {
@@ -41,32 +66,40 @@ int main() {
 
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
-            if (n > m) {
-                B[m][n] = 0.25;
-            } else {
+            if (m > n) {
                 B[m][n] = 0.75;
+            } else {
+                B[m][n] = 0.25;
             }
         }
     }
 
     for (int n = 0; n < N; n++) {
         for (int j = 0; j < J; j++) {
-            F[n][j] = 1.0/(n+j);
+            if (n + j == 0) {
+                F[n][j] = 1.0/(n+j+1);
+            } else {
+                F[n][j] = 1.0/(n+j);
+            }
+
         }
     }
 
+    // Step 5: Performing matrix addition operation
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
             C[m][n] = A[m][n] + B[m][n];
         }
     }
 
+    // Step 6: Performing scalar multiplication operation
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
             D[m][n] = k * A[m][n];
         }
     }
 
+    // Step 7: Performing matrix multiplication operation
     for (int m = 0; m < M; m++) {
         for (int j = 0; j < J; j++) {
             double sum = 0;
@@ -77,8 +110,9 @@ int main() {
         }
     }
 
+    // Step 8: Displaying the results in a professional way
     std::cout << "------------------------------RESULTS-------------------------" << std::endl;
-
+    std::cout << "Matrix C (A + B):" << std::endl;
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
             std::cout << C[m][n] << " ";
@@ -86,16 +120,18 @@ int main() {
         std::cout << std::endl;
     }
 
+    std::cout << "\nMatrix D (k * A):" << std::endl;
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
-            std::cout << D[m][n] << " ";
+            std::cout << std::setprecision(2) << D[m][n] << " ";
         }
         std::cout << std::endl;
     }
 
+    std::cout << "\nMatrix E (A * F):" << std::endl;
     for (int m = 0; m < M; m++) {
         for (int j = 0; j < J; j++) {
-            std::cout << E[m][j] << " ";
+            std::cout << std::setprecision(2) << E[m][j] << " ";
         }
         std::cout << std::endl;
     }
