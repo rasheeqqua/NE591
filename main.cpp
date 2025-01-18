6027b592f7e8ef744df021020b512b8baeba7874
Hasibul H Rasheeq <hrashee@ncsu.edu>
2025-01-17 22:27:06 -0500

    - Iteration 1

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
-/*--------------------In-Lab-2--------------------*/
-/*
- * Created by Hasibul H. Rasheeq on Friday, 1/17/25.
-*/
-
 #include <algorithm>
-#include <functional>
-#include <bits/stdc++.h>
-#include "function.cpp"
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <bits/std_function.h>
+#include "function.cpp" // for evaluate()
 
 int main() {
     std::cout << "          NE-591 IN-LAB ASSIGNMENT 2          " << std::endl;
     std::cout << "          Credits: Hasibul H. Rasheeq, NCSU          " << std::endl;
     std::cout << "          Jan 17, 2025          " << std::endl;
-    std::cout << "\n\n          LAGRANGE INTERPOLATION PROGRAM          " <<std::endl;
+    std::cout << "\n\n          LAGRANGE INTERPOLATION PROGRAM          " << std::endl;
     std::cout << "************************************************ \n\n" << std::endl;
 
     int n, m, choice;
-    std::vector<double> x, y, step_values;
+    std::vector<double> x;
+    std::vector<double> y;
+    std::map<double, double> xy;
 
     std::cout << "Number of interpolation points, n: ";
     std::cin >> n;
@@ -31,7 +28,7 @@ int main() {
         return 1;
     }
 
-    std::cout << "Interpolation points, x: " << std::endl;
+    std::cout << "Please enter your " << n << " interpolation points, x: " << std::endl;
     for (int i = 0; i < n; i++) {
         double temp;
         std::cin >> temp;
@@ -39,35 +36,50 @@ int main() {
     }
     std::sort(x.begin(), x.end());
 
-    std::cout << "Option 1: Enter the values from a text file.";
-    std::cout << "Option 2: Calculate the values from f(x)." << std::endl;
-    std::cout << "P.S.: You can change the function by editing line 11 of function.cpp file" << std::endl;
+    std::cout << "\nOption 1: Read ALL (x, y) pairs from a text file.\n";
+    std::cout << "Option 2: Use the user-provided x-values and compute y = f(x) (currently exp(x)).\n";
+    std::cout << "P.S.: You can change f(x) by editing function.cpp.\n";
     std::cout << "Enter your choice: ";
     std::cin >> choice;
+
     if (choice == 1) {
         std::string filename;
-        std::cout << "Enter the name of the file: " << std::endl;
+        std::cout << "Enter the name of the file: ";
         std::cin >> filename;
 
         std::ifstream file(filename);
         if (!file.is_open()) {
-            std::cout << "Sorry cannot open the file. Either the name of the file is wrong or it is corrupted." << std::endl;
+            std::cout << "Cannot open the file. Check name or file integrity." << std::endl;
             return 1;
         }
 
-        std::string s;
-        while (getline(file, s, '\n')) {
-            double evaluated_value = std::stod(s);
-            y.push_back(evaluated_value);
+        // We assume the file has lines like: -1.0, 0.37 (x, y)
+        std::string line;
+        while (std::getline(file, line)) {
+            std::stringstream ss(line);
+            double x_in_file, y_in_file;
+            char comma;
+
+            if (ss >> x_in_file) {
+                if (ss.peek() == ',') {
+                    ss >> comma;
+                }
+                if (ss >> y_in_file) {
+                    xy[x_in_file] = y_in_file;
+                }
+            }
         }
+        file.close();
+
     } else if (choice == 2) {
         y = evaluate(x);
+        for (size_t i = 0; i < x.size(); i++) {
+            xy[x[i]] = y[i];
+        }
     } else {
         std::cout << "Invalid choice." << std::endl;
         return 1;
     }
-
-    std::cout << "Looks like all the provided values are correct so far! Give me a few moments to return the interpolated values!" << std::endl;
 
     return 0;
 }
